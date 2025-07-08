a3b802c2a3800102830c03046dd928e631291422
sooryathejes <sooryathjs007@gmail.com>
2025-07-08 11:33:35 +0530

    .

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -1,3 +1,5 @@
+import { assets, workData } from "@/assets/assets";
+import Image from "next/image";
 import React from "react";
 
 const Work = () => {
@@ -9,13 +11,27 @@ const Work = () => {
         Welcome to my web development portfolio! Explore a collection of
         projects showcasing my expertise in front-end development.
       </p>
-      <div> 
-        {
-            workData.map
-        }                                                                           
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4  my-10 gap-5">
+        {workData.map((project, index) => (
+          <div
+            key={index}
+            className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group"
+            style={{ backgroundImage: `url(${project.bgImage})` }}
+          >
+            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+            <div>
+              <h2 className="font-semibold">{project.title} </h2>
+              <p className="text-sm text-gray-700">{project.description}</p>
+            </div>
+            <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+              <Image src={assets.send_icon} alt="send icon" className="w-5"/>
+            </div>
+            </div>
+            
+          </div>
+        ))}
       </div>
     </div>
   );
-};
-
-export default Work; 
+}; 
+export default Work;
