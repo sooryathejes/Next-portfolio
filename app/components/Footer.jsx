03b94896821c8b4baf403f99b50b87bc02b0782f
meghnadas <meghnadas2504@gmail.com>
2025-07-12 22:29:20 +0530

    .

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
                 <div className='w-max flex items-center gap-2 mx-auto'>
                     <Image src={assets.mail_icon} alt='' className='w-6'/>
                     greatstackdev@gmail.com
-                </div>
+                </div>   
             </div>
 
             <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
